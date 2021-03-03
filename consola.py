c3ff87f82dfdf0540a40e447be264044edfa891a
Maubg <68229077+maubg-debug@users.noreply.github.com>
2021-03-03 14:49:00 +0100

    Update consola.py

--- consola.py
+++ consola.py
@@ -4,25 +4,31 @@ import sys
 argv = sys.argv
 
 if len(argv) == 2:
-    file = argv[1].replace('.\\', '').replace('./', '')
-    result, error = maulen.run(f'<{file}>', f'run("{file}")')
 
-    if error:
-        print(error.como_string())
+  # file = argv[1].replace('.\\', '').replace('./', '')
+  
+  file = argv[1]
+  try:
+    result, error = maulen.run(f'<{file}>', f'run("{file}")')
+  except KeyboardInterrupt:
+    exit(0)
+
+  if error:
+      print(error.como_string())
 else:
     while True:
-        try:
-            texto = input('>>> ')
-            if texto.strip() == "":
-                continue
-            result, error = maulen.run('<stdin>', texto)
+      try:
+        texto = input('>>> ')
+        if texto.strip() == "":
+            continue
+        result, error = maulen.run('<stdin>', texto)
 
-            if error:
-                print(error.as_string())
-            elif result:
-                if len(result.elements) == 1:
-                    print(repr(result.elements[0]))
-                else:
-                    print(repr(result))
-        except KeyboardInterrupt:
-            print("\n - pon exit() para salir - \n")
+        if error:
+            print(error.as_string())
+        elif result:
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+      except KeyboardInterrupt:
+          print("\n - pon exit() para salir - \n")
